c2db69ce8d0f13ec65b7c45ed2564f9c3e39ffcd
aaron <33755873@qq.com>
2018-07-20 23:34:21 +0000

    Initial Commit

---
+++ app.js
@@ -0,0 +1,26 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        title: sample.title,
+        address: sample.address,
+        about: sample.about,
+        contracted: true,
+        modalOpen: false,
+        headerImageStyle: {
+            'background-image': 'url(sample/header.jpg)'
+        },
+        amenities: sample.amenities,
+        prices: sample.prices
+    },
+    watch: {
+        modalOpen: function() {
+            var className = 'modal-open';
+            if (this.modalOpen) {
+                document.body.classList.add(className);
+            } else {
+                document.body.classList.remove(className);
+            }
+        }
+    }
+});
+
