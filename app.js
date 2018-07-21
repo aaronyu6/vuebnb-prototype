c6a77bb27c93fe35efafe4780e927053c4bc6e37
Your Name <you@example.com>
2018-07-21 00:38:55 +0000

    prototype last code

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+function escapeKeyListener(evt) {
+    if (evt.keyCode === 27 && app.modalOpen) {
+        app.modalOpen = false;
+    }
+}
+
 var app = new Vue({
     el: '#app',
     data: {
@@ -21,6 +27,9 @@ var app = new Vue({
                 document.body.classList.remove(className);
             }
         }
+    },
+    created: function() {
+        document.addEventListener('keyup', escapeKeyListener);
     }
 });
 
